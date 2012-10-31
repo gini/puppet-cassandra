84f2af9e13eee9f8bffe7d73735430b78f805f5b
Casper Mout <casper@caspermout.nl>
2012-10-31 22:27:21 +0100

    added key option to apt::source, it does not add the key without this option

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -10,6 +10,7 @@ class cassandra::repo::debian(
         location          => $location,
         release           => $release,
         repos             => $repos,
+        key               => $repo_name,
         key_source        => $key_source,
         pin               => $pin,
         include_src       => false,
